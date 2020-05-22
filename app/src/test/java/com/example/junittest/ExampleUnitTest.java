757efd785449762f8e9f0630e459984642f703d6
sanada_a <lov2luf@gmail.com>
2020-05-22 17:50:38 +0900

    modified

--- app/src/test/java/com/example/junittest/ExampleUnitTest.java
+++ app/src/test/java/com/example/junittest/ExampleUnitTest.java
@@ -11,10 +11,12 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    //@Test
-    //public void addition_isCorrect() {
-    //    assertEquals(4, 2 + 2);
-    //}
+    /*
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+*/
 
     private Caluculator mCaluculator;
 
@@ -33,4 +35,5 @@ public class ExampleUnitTest {
     public void testSubstract() throws Exception {
         assertEquals(3, mCaluculator.substract(5,3), 0);
     }
+
 }

ae56130fe2f565d6480d1b2dd566c12e2cd4949b
sanada_a <lov2luf@gmail.com>
2020-05-22 18:46:54 +0900

    CaluculatorTestを追加

--- app/src/test/java/com/example/junittest/CaluculatorTest.java
+++ app/src/test/java/com/example/junittest/CaluculatorTest.java
@@ -5,18 +5,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    /*
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
-*/
+public class CaluculatorTest {
 
     private Caluculator mCaluculator;
 
@@ -35,5 +24,4 @@ public class ExampleUnitTest {
     public void testSubstract() throws Exception {
         assertEquals(3, mCaluculator.substract(5,3), 0);
     }
-
 }

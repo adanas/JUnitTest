f6a8918ad34cca8ff5e96ab3bcb3fb71c3a63d61
sanada_a <lov2luf@gmail.com>
2020-05-22 17:01:19 +0900

    新規追加

---
+++ app/src/androidTest/java/com/example/junittest/CaluculatorTest.java
@@ -0,0 +1,44 @@
+package com.example.junittest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CaluculatorTest {
+    /*
+    * @Before
+    *  メソッド名はsetUp()で定義されている。
+    *  初期化メソッド実行前の処理(メソッド毎に必ず呼ばれる)
+    *  引数を持たないpublicメソッドにする
+    * @Test
+    *  テストメソッドの宣言。
+    *  引数を持たないpublicメソッドにする
+    *  @Test(expected=Exception.class)で例外が出るテスト
+    * @After
+    *  メソッド名はtearDownで定義されている
+    *  後処理メソッド実行後の処理(メソッド毎に必ず呼ばれる)
+    *  引数を持たないpublicメソッドにする
+    *  必ず行わないといけない後処理を記載する
+    * @Ingnore
+    *  テスト実行除外
+    *  一時的に実行したくないときに使う
+     * */
+
+    private Caluculator mCaluculator;
+
+    @Before
+    public void setUp() throws Exception {
+        mCaluculator = new Caluculator();
+    }
+
+    @Test
+    public void testSum() throws Exception {
+        assertEquals(7, mCaluculator.sum(1,6), 0);
+    }
+
+    @Test
+    public void testSubstract() throws Exception {
+        assertEquals(3, mCaluculator.substract(5,3), 0);
+    }
+}
---
+++ app/src/main/java/com/example/junittest/Caluculator.java
@@ -0,0 +1,12 @@
+package com.example.junittest;
+
+public class Caluculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int substract(int a,int b){
+        return a - b;
+    }
+}
